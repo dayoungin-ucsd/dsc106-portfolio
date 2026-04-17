9c70ce076c32aab2ada07c630bd428ff74f9fe6e
dayoungin-ucsd <dain@ucsd.edu>
2026-04-17 03:26:53 -0700

    Lab3-Step4
    Dark mode!

--- global.js
+++ global.js
@@ -23,6 +23,19 @@ let pages = [
 let nav = document.createElement('nav');
 document.body.prepend(nav);
 
+document.body.insertAdjacentHTML(
+    'afterbegin',
+    `
+        <label class="color-scheme">
+            Theme:
+            <select>
+                <option value="light dark">Automatic</option>
+                <option value="light">Light</option>
+                <option value="dark">Dark</option>
+            </select>
+        </label>`,
+);
+
 const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
     ? "/"
     : "/dsc106-portfolio/";
@@ -50,3 +63,9 @@ for (let p of pages) {
 
     nav.append(a);
 }
+
+let select = document.querySelector('select');
+
+select.addEventListener('input', function (event) {
+    console.log('color scheme changed to', event.target.value);
+});
