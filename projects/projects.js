5aebcffc1ef479f0d5db40fad5155bf359c6e039
dayoungin-ucsd <dain@ucsd.edu>
2026-04-24 12:28:42 -0700

    Lab4-Step1
    Templating projects from a data file

--- projects/projects.js
+++ projects/projects.js
@@ -3,6 +3,8 @@ import { fetchJSON, renderProjects } from "../global.js";
 const projects = await fetchJSON('../lib/projects.json');
 
 const projectsContainer = document.querySelector('.projects');
-console.log('container:', projectsContainer);
 
 renderProjects(projects, projectsContainer, 'h2');
+
+const titleElement = document.querySelector('.projects-title');
+titleElement.textContent = `${projects.length} Projects`;
