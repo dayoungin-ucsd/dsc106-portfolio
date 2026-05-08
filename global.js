3a8b47de658628dd34587d471ce4036a7532e7fc
dayoungin-ucsd <dain@ucsd.edu>
2026-05-08 16:10:42 -0700

    Lab6-Step0
    Setting up

--- global.js
+++ global.js
@@ -17,6 +17,7 @@ let pages = [
     { url: 'projects/', title: 'Projects'},
     { url: 'contact/', title: 'Contact'},
     { url: 'resume/', title: 'Resume'},
+    { url: 'meta/', title: 'Meta'},
     { url: 'https://github.com/dayoungin-ucsd/', title: 'GitHub'}
 ]
 
