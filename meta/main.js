fa52593f8c80b2019d77f7232901542c725ed735
dayoungin-ucsd <dain@ucsd.edu>
2026-05-08 18:52:38 -0700

    Lab6-Step2
    Visualizing time and day of commits in a scatterplot

--- meta/main.js
+++ meta/main.js
@@ -111,6 +111,15 @@ function renderScatterPlot(data, commits) {
     xScale.range([usableArea.left, usableArea.right]);
     yScale.range([usableArea.bottom, usableArea.top]);
 
+    // Add gridlines BEFORE the axes
+    const gridlines = svg
+        .append('g')
+        .attr('class', 'gridlines')
+        .attr('transform', `translate(${usableArea.left}, 0)`);
+
+    // Create gridlines as an axis with no labels and full-width ticks
+    gridlines.call(d3.axisLeft(yScale).tickFormat('').tickSize(-usableArea.width));
+
     // Create the axes
     const xAxis = d3.axisBottom(xScale);
     const yAxis = d3
