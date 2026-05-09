7ca07cd6910bb2cefbd5c15eb3e742c5e372fd71
dayoungin-ucsd <dain@ucsd.edu>
2026-05-08 18:31:17 -0700

    Lab6-Step2
    Visualizing time and day of commits in a scatterplot

--- meta/main.js
+++ meta/main.js
@@ -66,9 +66,51 @@ function renderCommitInfo(data, commits) {
 
     dl.append('dt').text('Average line length');
     dl.append('dd').text(d3.mean(data, d => d.length).toFixed(2));
+
+    const workByPeriod = d3.rollups(
+    data,
+    (v) => v.length,
+    (d) => new Date(d.datetime).toLocaleString('en', { dayPeriod: 'short' }),
+    );
+
+    const maxPeriod = d3.greatest(workByPeriod, (d) => d[1])?.[0];
+
+    dl.append('dt').text('Most active time of day');
+    dl.append('dd').text(maxPeriod);
+}
+
+function renderScatterPlot(data, commits) {
+    const width = 1000;
+    const height = 600;
+
+    const svg = d3
+    .select('#chart')
+    .append('svg')
+    .attr('viewBox', `0 0 ${width} ${height}`)
+    .style('overflow', 'visible');
+
+    const xScale = d3
+    .scaleTime()
+    .domain(d3.extent(commits, (d) => d.datetime))
+    .range([0, width])
+    .nice();
+
+    const yScale = d3.scaleLinear().domain([0, 24]).range([height, 0]);
+
+    const dots = svg.append('g').attr('class', 'dots');
+
+    dots
+        .selectAll('circle')
+        .data(commits)
+        .join('circle')
+        .attr('cx', (d) => xScale(d.datetime))
+        .attr('cy', (d) => yScale(d.hourFrac))
+        .attr('r', 5)
+        .attr('fill', 'steelblue');
 }
 
 let data = await loadData();
 let commits = processCommits(data);
 
 renderCommitInfo(data, commits);
+renderScatterPlot(data, commits);
