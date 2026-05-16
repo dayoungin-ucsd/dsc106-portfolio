810ff0a0bc8a84b3897ccbded8827f164adfd32b
dayoungin-ucsd <dain@ucsd.edu>
2026-05-15 22:34:32 -0700

    add-project
    bikewatching

--- global.js
+++ global.js
@@ -120,8 +120,24 @@ export function renderProjects(projects, containerElement, headingLevel = 'h2') 
         const article = document.createElement('article');
 
         article.innerHTML = `
-            <${headingLevel}>${project.title}</${headingLevel}>
-            <img src="${BASE_PATH}${project.image}" alt="${project.title}">
+            <!--<${headingLevel}>${project.title}</${headingLevel}>
+            <img src="${project.image}" alt="${project.title}">-->
+
+            <${headingLevel}>
+                ${
+                    project.url
+                    ? `<a href="${project.url}" target="_blank">${project.title}</a>`
+                    : project.title
+                }
+            </${headingLevel}>
+            ${
+                project.url
+                ? `<a href="${project.url}" target="_blank">
+                        <img src="${BASE_PATH}${project.image}" alt="${project.title}">
+                   </a>`
+                : `<img src="${BASE_PATH}${project.image}" alt="${project.title}">`
+            }
+
             <div>
                 <p>${project.description}</p>
                 <p class="project-year">c. ${project.year}</p>
