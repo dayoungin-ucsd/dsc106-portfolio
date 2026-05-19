9fc2c646f6b6675a7cddd3df01d8d3db7559edeb
dayoungin-ucsd <dain@ucsd.edu>
2026-05-18 22:01:54 -0700

    Lab8-Step1
    Evolution visualization

--- meta/main.js
+++ meta/main.js
@@ -278,5 +278,33 @@ function createBrushSelector(svg) {
 let data = await loadData();
 let commits = processCommits(data);
 
+let commitProgress = 100;
+
+let timeScale = d3
+    .scaleTime()
+    .domain([
+        d3.min(commits, (d) => d.datetime),
+        d3.max(commits, (d) => d.datetime),
+    ])
+    .range([0, 100]);
+let commitMaxTime = timeScale.invert(commitProgress);
+
+function onTimeSliderChange() {
+    commitProgress = +document.getElementById('commit-progress').value;
+    commitMaxTime = timeScale.invert(commitProgress);
+
+    document.getElementById('commit-time').textContent =
+        commitMaxTime.toLocaleString('en', {
+            dateStyle: 'long',
+            timeStyle: 'short',
+        });
+}
+
+document
+    .getElementById('commit-progress')
+    .addEventListener('input', onTimeSliderChange);
+
+onTimeSliderChange();
+
 renderCommitInfo(data, commits);
 renderScatterPlot(data, commits);
